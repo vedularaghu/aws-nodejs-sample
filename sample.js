df4cbb01b5dbb43ea5250fe993463deea71d810d
Raghavendra Vedula <vedularaghavendra03@gmail.com>
2017-12-19 15:54:18 +0530

    file added in a bucket

--- sample.js
+++ sample.js
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,10 +22,9 @@ var uuid = require('node-uuid');
 var s3 = new AWS.S3();
 
 // Create a bucket and upload something into it
-var bucketName = 'node-sdk-sample-' + uuid.v4();
+var bucketName = 'incomingeventsmytrah';
 var keyName = 'hello_world.txt';
 
-s3.createBucket({Bucket: bucketName}, function() {
   var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
   s3.putObject(params, function(err, data) {
     if (err)
@@ -33,4 +32,3 @@ s3.createBucket({Bucket: bucketName}, function() {
     else
       console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
   });
-});
